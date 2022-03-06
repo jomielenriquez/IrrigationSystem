19b7c0be7fda264526bcfc0c3d847a314eacea5e
Jomiel Enriquez <enriquez.jliquigan@gmail.com>
2022-03-06 13:01:50 +0800

    add Ultrasonic and DHT11 to SMS and weather nano

--- SMSOnly/Nano-SMS/Nano-SMS.ino
+++ SMSOnly/Nano-SMS/Nano-SMS.ino
@@ -1,10 +1,10 @@
 
 #include<SoftwareSerial.h>
-const int AirValue1 = 300; //285   
-const int SWSValue1 = 190;  
+const int AirValue1 = 610; //285   
+const int SWSValue1 = 302;  
 
-const int AirValue2 = 300; //280  //you need to replace this value with Value_1
-const int SWSValue2 = 190;  //you need to replace this value with Value_2
+const int AirValue2 = 606; //280  
+const int SWSValue2 = 286;  
 int intRecord=0;
 int soilMoistureValue1 = 0,soilMoistureValue2 = 0,soilMoistureValue3 = 0;
 int Ave=0;
@@ -12,7 +12,6 @@ int soilmoisturepercent=0;
 bool isWaterOn=false,isPause =  false, isHour=false;
 int intIsWaterOn=0, intIsPause=0, intWater=0;
 String strWeather = "";
-int intWeather = 0;
 bool willRain = false;
 
 SoftwareSerial sw(5,6);  //RX,TX
@@ -23,38 +22,13 @@ void setup() {
   digitalWrite(8,HIGH);
 }
 void loop() {
-  while (sw.available()){
-    strWeather = sw.readStringUntil('\n');
-    Serial.println();
-    Serial.println("\n=====================");
-    Serial.println(strWeather);
-    intWeather = strWeather.toInt();
-    Serial.println("=====================\n");
-    if(intWeather>=200 && intWeather <=232){
-      willRain = true;
-      Serial.print("WILL RAIN\n");
-    }
-    else if(intWeather>=300 && intWeather<=321){
-      willRain = true;
-      Serial.print("WILL RAIN\n");
-    }
-    else if(intWeather>=500 && intWeather<=531){
-      willRain = true;
-      Serial.print("WILL RAIN\n");
-    }
-    else{
-      willRain = false;
-      Serial.print("WILL NOT RAIN\n");
-    }
-    
-  }
   //Serial.println("Reading1: ");
-  soilMoistureValue1 = analogRead(A2);  //put Sensor insert into soil
-  soilMoistureValue2 = soilMoistureValue1;  //put Sensor insert into soil
-  soilMoistureValue3 = analogRead(A3);  //put Sensor insert into soil
+  soilMoistureValue1 = analogRead(A1);  //put Sensor insert into soil
+  soilMoistureValue2 = analogRead(A2);  //put Sensor insert into soil
+  //soilMoistureValue3 = analogRead(A3);  //put Sensor insert into soil
 
   int SM_per1 = map(soilMoistureValue1,AirValue1,SWSValue1,0,100);
-  int SM_per2 = map(soilMoistureValue1,AirValue2,SWSValue2,0,100);
+  int SM_per2 = map(soilMoistureValue2,AirValue2,SWSValue2,0,100);
 
   Ave = (SM_per1+SM_per2)/2;
   
@@ -62,35 +36,36 @@ void loop() {
   //if(intRecord==300000)
   //if(intRecord==3000){
     //intRecord=0;
-    Serial.print("\n===========\nSaved to SD\n===========\n");
+    Serial.print("=>SD ");
     sw.println("Reading1 : " + String(soilMoistureValue1) + " - " + SM_per1 + "%, Reading2 : " + String(soilMoistureValue2) + " - " + SM_per2 + "%, Average: " + String(Ave) + "%, isWaterOn : "  + String(intIsWaterOn));
   //}
 //3600000
-  Serial.print(intWater);
-  if(intWater==3000){
+  Serial.print(intWater/1000);
+  if(intWater==10000){
     intWater=0;
     isHour=true;
   }
   
   if(isHour==true){
-    Serial.print("One hour");
+    Serial.print("1hr");
   
     if(isPause==false)
     {
-      if(Ave<80 && willRain == false)
-      //if(Ave<50)
+      if(Ave<50)
       {
         isWaterOn = true;
         digitalWrite(8,LOW);
       }
       else{
-        isHour=false;
+        //isHour=false;
       }
+      //if(Ave>=80 && intIsWaterOn == 18 && isWaterOn == true){
       if(intIsWaterOn == 3 && isWaterOn == true){
         intIsWaterOn=0;
         isWaterOn = false;
         digitalWrite(8,HIGH);
         isPause=true;
+        isHour=false;
       }
       
       if (isWaterOn == true){
